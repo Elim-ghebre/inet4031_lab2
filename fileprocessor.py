76a7cf52f5ea6608029c60f64e94d19888716d30
elimghebregzabiaher <109236996+elimghebregzabiaher@users.noreply.github.com>
2023-09-23 13:51:09 -0500

    Update fileprocessor.py

--- fileprocessor.py
+++ fileprocessor.py
@@ -1,16 +1,18 @@
-file = open("fileprocessor.input", 'r')
-lines = file.readlines()
-file.close()
+with open("fileprocessor.input", 'r') as file:
+    lines = file.readlines()
 
 print("Printing out User Data:\n")
 
 for line in lines:
-    line = line.strip()
+    line = line.strip()  
 
-    if line[0] == "#":
-        print(line[1:], "is skipped because it starts with a hashtag (is commented out)")
+    if line.startswith("#"):
+        user = line.split(":")[0][1:]
+        print(user, "is skipped because it starts with a hashtag (is commented out)")
     else:
         parts = line.split(":")
-        print("The user", parts[0], "has a password of", parts[1], "and has userid", parts[2], "and groupid", parts[3])
+
+        if len(parts) == 4:
+            print("The user", parts[0], "has a password of", parts[1], "and has userid", parts[2], "and groupid", parts[3])
 
 print("\nEnd of User Data")
